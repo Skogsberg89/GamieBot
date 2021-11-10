9cb035086a4ee8c185411f5fd3653f8dd853cf01
Skogsberg89 <h.skogsberg89@gmail.com>
2021-11-10 19:06:17 +0100

    did the two first unit tests

--- src/test/java/com/gamiebot/GamiebotApplicationTests.java
+++ src/test/java/com/gamiebot/GamiebotApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GamiebotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
--- src/test/java/com/gamiebot/listeners/commands/ExecuteCommandsTest.java
+++ src/test/java/com/gamiebot/listeners/commands/ExecuteCommandsTest.java
@@ -12,8 +12,9 @@ class ExecuteCommandsTest {
     @Test
     @DisplayName("Return help Message")
     void help() {
-        //Start
+        //Given
         con = new Controller(".gamie help", "testbot");
+        //Expect
         EXPECT_STRING = "So " + con.getName() + " what can i help you with?";
         assertEquals(EXPECT_STRING, con.getReturnMessage());
     }
@@ -21,8 +22,10 @@ class ExecuteCommandsTest {
     @Test
     @DisplayName("Steam news with no game in input")
     void steamNewsNoGameInput() {
+        //Given
         con = new Controller(".gamie -steam news", "testbot");
-        EXPECT_STRING = con.getName() + " Which game do you want news of?\n .game -steam news <game> ";
+        //Expect
+        EXPECT_STRING = con.getName() + " which game do you want news of?\n .game -steam news <game> ";
         assertEquals(EXPECT_STRING, con.getReturnMessage());
     }
 }
